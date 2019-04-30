aa213060d5bc02e75262c95334103ac878dba533
multimrkvica <47330210+multimrkvica@users.noreply.github.com>
2019-04-30 22:54:49 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
-import axios from "axios";
-import $ from "jquery";/////gde kosristim ta dva stavljam ih u js file
+import axios from 'axios';
+import $ from 'jquery'; /////gde kosristim ta dva stavljam ih u js file
+$(document).ready()
+    // // navbar-scroll
+var scroll = new SmoothScroll('a[href*="#"][data-scroll]');
+
+// galerija
+
+// $(function() {
+//     $("#galerija").load("mdb-addons/galerija");
+// });
